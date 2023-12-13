bd6f25590aced56450487cc6449115296f9af883
Ranj8521Kumar <22it3037@rgipt.ac.in>
2023-12-13 19:29:51 +0530

    project first commit

---
+++ project 2/project2.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout << "Your most Welcome!" << endl;
+    cout << "How can I Help You?" << endl;
+    while (true)
+    {
+        int choice;
+        cout << "select any option from below: " << endl;
+        cout << "1.Add new Vehicle" << endl;
+        cout << "2.View Veiche Details" << endl;
+        cout << "3.Edit Vehicle Details" << endl;
+        cout << "4.Add new User" << endl;
+        cout << "5.Add new Trip" << endl;
+        cout << "6.View Trip" << endl;
+        cout << "7.Start Trip" << endl;
+        cout << "8.Complete Trip" << endl;
+        cout << "9..Exit" << endl;
+        cout<<"Enter Your choice: ";
+        cin>>choice;
+        switch (choice)
+        {
+        case 1:
+        {
+            break;
+        }
+
+        case 2:
+        {
+            break;
+        }
+
+        case 3:
+        {
+            break;
+        }
+
+        case 4:
+        {
+            break;
+        }
+
+        case 5:
+        {
+            break;
+        }
+
+        case 6:
+        {
+            break;
+        }
+
+        case 7:
+        {
+            break;
+        }
+
+        case 8:
+        {
+            break;
+        }
+
+        case 9:
+        {
+            return 0;
+            break;
+        }
+        default:
+            cout << "Your choice is Wrong, Please Try Again!" << endl;
+            break;
+            return 0;
+        }
+    }
+}
